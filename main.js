ffb47b507bd4d0cd47f4cfdde68fcae8659de918
k-ravuri <kaviravuri888@gmail.com>
2018-08-11 13:06:59 -0700

    reorganized code

--- main.js
+++ main.js
@@ -17,9 +17,12 @@ document.getElementById('SignUp').addEventListener('submit', submitForm);
 
 // Submit form
 function submitForm(e){
-  e.preventDefault();
-
-
+  // e.preventDefault();
+  // var file = e.target.files[0];
+  // var storageRef = firebase.storage().ref();
+  // var uploadRef = storageRef.child("company_logos/" + file.name);
+  // console.log(file.name)
+  // uploadRef.put(file);
   var fname = getInputVal('fname').toLowerCase();
   var lname = getInputVal('lname').toLowerCase();
   var company = getInputVal('company').toLowerCase();
@@ -43,6 +46,46 @@ function submitForm(e){
   // Clear form
   document.getElementById('SignUp').reset();
 }
+
+$(function () {
+      $("#slider4").responsiveSlides({
+        auto: true,
+        pager:true,
+        nav:false,
+        speed: 400,
+        namespace: "callbacks",
+        before: function () {
+          $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+          $('.events').append("<li>after event fired.</li>");
+        }
+      });
+    });
+    window.onload = function() {
+    var realBtn = document.getElementById("file");
+    var customBtn = document.getElementById("file-button");
+    var customTxt = document.getElementById("button-text");
+    
+    customBtn.addEventListener("click", function() {
+        realBtn.click();
+    });
+    
+    realBtn.addEventListener("change", function() {
+        if (realBtn.value) {
+            customTxt.innerHTML = realBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+        } else {
+            customTxt.innerHTML = "No File Chosen" ;
+        }
+    });
+    document.getElementById('SignUp').addEventListener('submit', function(e){
+        var storageRef = firebase.storage().ref();
+        var uploadRef = storageRef.child(realBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]);
+        console.log(realBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]);
+        uploadRef.put(realBtn.value[0]);
+
+      });
+    }
 
 // Function to get get form values
 function getInputVal(id){
